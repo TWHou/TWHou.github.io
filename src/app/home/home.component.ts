71d5bd3be8d0e959e3829e070e8b595de800c934
Christina Hsieh <hsiehkunlun@hotmail.com>
2017-10-08 02:20:19 -0400

    feat: setup home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  project = {
+    name: 'Voting App',
+    image: 'https://images.unsplash.com/photo-1494172892981-ce47ca685eea',
+    stack: ['react', 'react-router', 'express', 'mangodb'],
+    info: 'voting app details'
+  };
+
   constructor() { }
 
   ngOnInit() {
