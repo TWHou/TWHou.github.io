b703c5a14c1a4d38281a35154eb0ecb6b9a288e2
Christina Hsieh <hsiehkunlun@hotmail.com>
2017-10-18 05:04:29 -0400

    feat: setup projects component

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Project } from '../shared/project';
+import { ProjectService } from '../service/project.service';
+
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  apps: Project[];
+  apis: Project[];
+  selected = 'app';
+  loading = true;
+  baseURL = 'assets/';
+
+  constructor(
+    private projectService: ProjectService
+  ) { }
 
   ngOnInit() {
+    this.projectService.getProjects().then(projects => {
+      this.apps = projects.filter(project => project.type === 'app');
+      this.apis = projects.filter(project => project.type === 'api');
+      this.loading = false;
+    });
+  }
+
+  select(filter) {
+    this.selected = filter;
   }
 
 }
